26b89888d4818052cad7f80f1e4bd86702ef19d9
OFCL08 <fcarrillo@arkusnexus.com>
2021-11-21 12:44:12 -0800

    Ticket-01 Bugifx Sending messages

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -5,10 +5,12 @@ import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
-
+  const sortedMessages = messages.sort((message, nextMessage) =>
+    Date.parse(message.createdAt) - Date.parse(nextMessage.createdAt));
+  
   return (
     <Box>
-      {messages.map((message) => {
+      {sortedMessages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
